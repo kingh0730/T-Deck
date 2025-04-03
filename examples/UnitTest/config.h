1c3550bb9e13499c78671e3f8ff2be813f7bf2e1
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2025-04-03 02:12:11 -0700

    good shit

--- examples/UnitTest/config.h
+++ examples/UnitTest/config.h
@@ -1,17 +1,13 @@
 #pragma once
 
-
 #ifndef WIFI_SSID
-#define WIFI_SSID             "You WIFI SSID"
+#define WIFI_SSID "SKYLOW"
 #endif
 
 #ifndef WIFI_PASS
-#define WIFI_PASS         "You WIFI PASSWORD"
+#define WIFI_PASS "skylow123"
 #endif
 
-#define NTP_SERVER1           "pool.ntp.org"
-#define NTP_SERVER2           "time.nist.gov"
-#define DEFAULT_TIMEZONE      "CST-8"         //When the time zone cannot be obtained, the default time zone is used
-
-
-
+#define NTP_SERVER1 "pool.ntp.org"
+#define NTP_SERVER2 "time.nist.gov"
+#define DEFAULT_TIMEZONE "CST-8" // When the time zone cannot be obtained, the default time zone is used
