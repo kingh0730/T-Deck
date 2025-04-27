6a94ee306a09349d43f25247e61957213591c106
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2025-04-27 13:29:09 -0700

    sure

--- src/classifier/_loop.cpp
+++ src/classifier/_loop.cpp
@@ -1,9 +1,9 @@
 #include "_loop.h"
+#include "classifier.h"
 
 void _loop(TFT_eSPI &tft)
 {
-    tft.setTextColor(random(0xFFFF));
-    tft.fillScreen(TFT_BLACK);
-    tft.drawString("Hello loop", random(5, 320), random(5, 240));
     delay(5000);
+
+    classify();
 }
---
+++ src/classifier/classifier.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+float train[][2] = {
+    {0, 0},
+    {1, 2},
+    {2, 4},
+    {3, 6},
+    {4, 8},
+};
+
+float rand_float(void)
+{
+    return (float)rand() / (float)(RAND_MAX);
+}
+
+void classify(void)
+{
+    float w = rand_float() * 10;
+    printf("w: %f\n", w);
+}
