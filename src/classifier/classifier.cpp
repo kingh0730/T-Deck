36b8e2e8f10652cbed133413e6a51078b6a32cd4
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2025-04-27 13:40:45 -0700

    great

--- src/classifier/classifier.cpp
+++ src/classifier/classifier.cpp
@@ -8,6 +8,8 @@ float train[][2] = {
     {4, 8},
 };
 
+#define TRAIN_SIZE (sizeof(train) / sizeof(train[0]))
+
 float rand_float(void)
 {
     return (float)rand() / (float)(RAND_MAX);
@@ -17,6 +19,20 @@ void classify(void)
 {
     srand(730);
 
-    float w = rand_float() * 10;
+    float w = rand_float() * 10.0f;
     printf("w: %f\n", w);
+
+    float error = 0.0f;
+    for (size_t i = 0; i < TRAIN_SIZE; i++)
+    {
+        float x = train[i][0];
+        float y = train[i][1];
+        float z = x * w;
+        float d = z - y;
+        error += d * d;
+        printf("x: %f, y: %f, z: %f, d: %f\n", x, y, z, d);
+    }
+
+    error /= TRAIN_SIZE;
+    printf("error: %f\n", error);
 }
